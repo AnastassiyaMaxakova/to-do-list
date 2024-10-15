3ace55697392fd87580bcd4e9ca4b6c07af01910
AnastassiyaMaxakova <anastassiya.maxakova@gmail.com>
2024-10-15 18:03:56 +0500

    Add and style modal for adding a task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const modal = document.querySelector("#modal");
+const openModals = document.querySelectorAll("#addTaskBtn");
+const closeModal = document.querySelector("#closeBtn");
+openModals.forEach(openModal => openModal.addEventListener("click", () => {
+    modal.showModal();
+    
+}));
+
+closeModal.addEventListener("click", () => {
+    document.getElementById("formTask").reset();
+    modal.close();
+});
