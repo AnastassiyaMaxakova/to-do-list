dde02715c8caef6ba826212ac7e308205f09e5b4
AnastassiyaMaxakova <anastassiya.maxakova@gmail.com>
2024-10-16 15:55:28 +0500

    Add template for each nav item

--- src/index.js
+++ src/index.js
@@ -12,3 +12,16 @@ closeModal.addEventListener("click", () => {
     document.getElementById("formTask").reset();
     modal.close();
 });
+
+
+const addProject = document.querySelector("#addProjectBtn");
+const addProjectContainer = document.getElementById('nav-project-add')
+addProject.addEventListener('click', () => {
+    addProjectContainer.classList.remove("hidden");
+});
+
+
+const closeProject = document.querySelector("#closeProjectBtn");
+closeProject.addEventListener('click', () => {
+    addProjectContainer.classList.add('hidden');
+});
